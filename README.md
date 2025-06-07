0519aa42e07dac85de51024b6c148f554c6f877b
Vishal <hellovishal2020@gmail.com>
2025-06-07 15:58:30 +0530

    Added main.go file along with readme file which are functional and understandable

---
+++ README.md
@@ -0,0 +1,22 @@
+### Flashcard app
+- This is a simple flashcard application mainly made to understand the API implementation on golang
+- I have implemented multiple paths for different operations in this
+- POST /card : will add a flashcard
+- GET /quiz : will randmonly get a flashcard's question 
+
+
+## How to run the application
+1. go run main.go (and we will get a response like the application is running)
+2. use different methods like POST and GET to view different results
+3. before using the GET method, populate the map with different flashcards.
+
+- Example usecase for adding a card:
+```bash
+curl -X POST \
+-d {"Question":"Who is the Prime Minister of India","Answer":"Narendra Modi"} \
+localhost:8080/card
+```
+- Example usecase for getting a random card:
+```bash
+curl localhost:8080/quiz
+```
---
+++ main.go
@@ -0,0 +1,70 @@
+// Features:
+//  POST /card : will add a flashcard
+//  GET /quiz : will randmonly get a flashcard's question
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Flashcard struct {
+	Question string
+	Answer   string
+}
+
+var (
+	Flashcards = make(map[int][]Flashcard)
+	counter    = 0
+	mu         sync.Mutex
+)
+
+func main() {
+	http.HandleFunc("/card", addCard)
+	http.HandleFunc("/quiz", randomCardGenerator)
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func addCard(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var card Flashcard
+		err := json.NewDecoder(r.Body).Decode(&card)
+		if err != nil || card.Question == "" || card.Answer == "" {
+			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
+			return
+		}
+		mu.Lock()
+		Flashcards[counter] = append(Flashcards[counter], card)
+		counter++
+		mu.Unlock()
+		json.NewEncoder(w).Encode(card)
+		return
+	} else {
+		fmt.Fprintln(w, "Wrong method used", http.StatusMethodNotAllowed)
+		return
+	}
+}
+func randomCardGenerator(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+		randomNumber := rng.Intn(len(Flashcards))
+		if len(Flashcards) == 0 {
+			fmt.Fprintf(w, "Please add any flashcards before trying to access them.")
+			return
+		}
+		randomcard := Flashcards[randomNumber]
+		json.NewEncoder(w).Encode(randomcard)
+		return
+
+	} else {
+		http.Error(w, "Wrong method used", http.StatusMethodNotAllowed)
+		return
+	}
+}
